a9fca406f19257922d843f217acded54aebc6584
lauranadolski <lauranadolski@users.noreply.github.com>
2018-10-16 16:19:25 -0400

    Add nav bar

---
+++ personal-website/src/components/NavigationBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NavigationBar extends React.Component {
+
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default NavigationBar;
